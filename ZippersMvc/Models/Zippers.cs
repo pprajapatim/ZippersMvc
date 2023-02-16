f1baf339211a103954553093830db4fc8f57fdd6
pprajapatim <113458151+pprajapatim@users.noreply.github.com>
2023-02-16 02:40:03 -0500

    update

---
+++ ZippersMvc/Models/Zippers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ZippersMvc.Models
+{
+    public class Zippers
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+        public string Durability { get; set; }
+        public string WaterResistance { get; set; }
+        public string Strength { get; set; }
+        public string Flexibility { get; set; }
+        
+    }
+}
